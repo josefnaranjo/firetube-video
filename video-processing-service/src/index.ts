c97dbf136569d0dedb20ee70bad2699801aeab56
Jose Naranjo <jfnaappus@gmail.com>
2024-02-05 21:35:28 -0800

    v1.0 - Docker file, video processing 360p

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -5,31 +5,28 @@ const app = express();
 app.use(express.json());
 
 app.post('/process-video', (req, res) => {
-
-  // Get the path of the input video file from the request body
+  //Get past of the input video from the req body:
   const inputFilePath = req.body.inputFilePath;
   const outputFilePath = req.body.outputFilePath;
 
-  // Check if the input file path is defined
   if (!inputFilePath || !outputFilePath) {
-    return res.status(400).send('Bad Request: Missing file path');
+    res.status(400).send("Bad Request: Missing file path.")
   }
 
-  // Create the ffmpeg command
   ffmpeg(inputFilePath)
-    .outputOptions('-vf', 'scale=-1:360') // 360p
-    .on('end', function() {
-        console.log('Processing finished successfully');
-        res.status(200).send('Processing finished successfully');
+    .outputOptions("-vf", "scale=-1:360") //360p
+    .on("end", () => {
+      return res.status(200).send("Processing finished successfully!");
     })
-    .on('error', function(err: any) {
-        console.log('An error occurred: ' + err.message);
-        res.status(500).send('An error occurred: ' + err.message);
+    .on("error", (err) => {
+      console.log(`An error occurred: ${err.message}`);
+      res.status(500).send(`Error processing video: ${err.message}`);
     })
-    .save(outputFilePath);
+    .save(outputFilePath)
 });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(
+    `Video processing service listening at http://localhost:${port}`);
 });
