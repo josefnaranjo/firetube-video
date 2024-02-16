eea850f47d57f6826946d161f78b61627decef0d
Jose Naranjo <jfnaappus@gmail.com>
2024-02-15 21:57:57 -0800

    v1.2 - Created components using Next.js (Navbar and Watch). Also, I set up Firebase

---
+++ ft-web-client/app/firebase/firebase.ts
@@ -0,0 +1,52 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { 
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  User 
+} from "firebase/auth";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA4DGxS4higIjzKno_iQjLMVw2gnMeOpq8",
+  authDomain: "firetube-video-9b4e0.firebaseapp.com",
+  projectId: "firetube-video-9b4e0",
+  appId: "1:263053075100:web:57115ef427612558068359",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+const auth = getAuth(app);
+
+/**
+ * Signs the user in with a Google popup.
+ * @returns A promise that resolves with the user's credentials.
+ */
+export function signInWIthGoogle() {
+  return signInWithPopup(auth, new GoogleAuthProvider());
+}
+
+/**
+ * Signs the user out.
+ * @returns A promise that resolves when the user is signed out.
+ */
+export function signOut() {
+  return auth.signOut();
+}
+
+/**
+ * Trigger a callback when user auth state changes.
+ * @returns A function to unsubscribe callback.
+ */
+
+export function onAuthStateChangedHelper(callback:  (user: User | null) => void) {
+  const unsubscriber = onAuthStateChanged(auth, callback);
+  // Return unsubscribe helper so we can clean up this listener if needed.
+  return () => unsubscriber();
+}
---
+++ ft-web-client/app/navbar/navbar.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image"
+import Link from 'next/link'
+import styles from "./navbar.module.css"
+
+export default function Navbar() {
+    return (
+        <nav className={styles.nav}> 
+            <Link href="/" >
+                <Image width={90}  height={20}
+                    src="/youtube-logo.svg" alt="Youtube Logo" />
+            </Link>
+        </nav>
+    )
+}
